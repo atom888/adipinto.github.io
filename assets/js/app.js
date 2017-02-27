0b9432840402952879790d983f846258560aa26f
Adam <adam.dipinto@gmail.com>
2017-02-27 03:37:19 +0000

    Minor JS but may not use

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+// $(() => {
+//
+//   var nav = $("nav");
+//
+//   $(window).scroll(function() {
+//     if($(this).scrollTop()) {
+//       $(nav).css('background-color', 'none')
+//     } else {
+//       $(nav).css('background-color', 'black');
+//     }
+//   })
+// })
